b04148ad00a9e1e2ce821a96a6c28512a333a562
FriendlyBurrito <paulius1207@gmail.com>
2017-04-08 01:10:49 +0300

    Update sketch.js

--- Tree/sketch.js
+++ Tree/sketch.js
@@ -1,89 +1,83 @@
 
   var axiom ="F";
-  var sentence = axiom;
-  var len = 100;
-  var rules = [];
-  var angle;
-  var sound;
-  var timer=0;
-  rules[0] = {
-  a: "F",
-  b: "FF+[+F-F-F]-[-F+F+F]"
+var sentence = axiom;
+var len = 100;
+var rules = [];
+var angle;
+var sound;
+var timer=0;
+rules[0] = {
+a: "F",
+b: "FF+[+F-F-F]-[-F+F+F]"
 }
 
 
 
 function generate(){
 len *= 0.56;
-  var nextSentence ="";
-    for (var i =0; i< sentence.length; i++){
-        var found=false;
-        var current = sentence.charAt(i);
-        for (var j=0; j<rules.length; j++){
-            if (current == rules[j].a){
-              found=true;
-              nextSentence += rules[j].b;
-              break;
-            }
-        }
+var nextSentence ="";
+  for (var i =0; i< sentence.length; i++){
+      var found=false;
+      var current = sentence.charAt(i);
+      for (var j=0; j<rules.length; j++){
+          if (current == rules[j].a){
+            found=true;
+            nextSentence += rules[j].b;
+            break;
+          }
+      }
 
-        if (!found){
-          nextSentence += current;
-        }
-    }
+      if (!found){
+        nextSentence += current;
+      }
+  }
 
-    sentence = nextSentence;
-  //  createP(sentence);
-    turtle();
+  sentence = nextSentence;
+//  createP(sentence);
+  turtle();
 }
 
 function turtle(){
-  resetMatrix();
+resetMatrix();
 //  background(0);
-  translate(width / 2, height);
-  stroke(200,200,200);
-  for (var i =0; i<sentence.length; i++){
-    var current = sentence.charAt(i);
+translate(width / 2, height);
+stroke(0,0,0);
+for (var i =0; i<sentence.length; i++){
+  var current = sentence.charAt(i);
 
-    if(current == "F"){
-      line(0,0,0,-len);
-      translate(0, -len);
-    }
-    else if ( current == "+"){
-      rotate(angle);
-    }
-    else if ( current == "-"){
-      rotate(-angle);
-    }
-    else if ( current == "["){
-      push();
+  if(current == "F"){
+    line(0,0,0,-len);
+    translate(0, -len);
+  }
+  else if ( current == "+"){
+    rotate(angle);
+  }
+  else if ( current == "-"){
+    rotate(-angle);
+  }
+  else if ( current == "["){
+    push();
 
-    }
-    else if ( current == "]"){
-      pop();
+  }
+  else if ( current == "]"){
+    pop();
 
-    }
   }
 }
-/*
-var pressed=0;
-function mousePressed() {
-  if (pressed <4){
-   sound.play();
-   generate();
-   pressed++;
- }
-}*/
+}
+
+var pressed=false;
+
 
 function setup() {
-  angle = radians(24);
-  sound=loadSound('pop.mp3');
+angle = radians(24);
+sound=loadSound('pop.mp3');
 var canvas = createCanvas(window.innerWidth, window.innerHeight);
-  canvas.position(0,0);
-  // background(5);
+canvas.position(0,0);
+// background(5);
 
 
-  turtle();
+if (pressed) turtle();
 
 //var button;
 //button = createButton('Generate');
@@ -92,7 +86,7 @@ var canvas = createCanvas(window.innerWidth, window.innerHeight);
 
 
 function draw() {
-timer++;
+if  ( pressed)timer++;
 if (timer == 20) generate();
 if (timer == 25) generate();
 if (timer == 30) generate();
@@ -100,3 +94,6 @@ if (timer == 35) generate();
 
 }
 
+function mousePressed() {
+pressed=true;
+}
